5024ea70c63345f26e4542af94460d1b32da1efc
Sunjay Varma <varma.sunjay@gmail.com>
2017-09-23 20:36:43 -0400

    Fixed bug that was causing speed to not be respected

--- examples/drawtest.rs
+++ examples/drawtest.rs
@@ -270,7 +270,8 @@ mod canvas {
                                     let speed = speed.to_absolute(); // px per second
                                     let path_line = math::sub(start, end);
                                     let path_length = math::square_len(path_line).sqrt();
-                                    let direction = math::mul_scalar(path_line, path_length);
+                                    let direction = math::mul_scalar(path_line, 1./path_length);
+                                    debug_assert_eq!(math::square_len(direction).sqrt(), 1.);
                                     // (px / sec) * (sec / 1000ms) * ms => px
                                     let offset = math::mul_scalar(direction, speed / 1000. * elapsed);
                                     let offset_length = math::square_len(offset).sqrt();
