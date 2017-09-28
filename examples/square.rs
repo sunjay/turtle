351542b90d6bc691c98a8951a55d1d6df4c5f620
Sunjay Varma <varma.sunjay@gmail.com>
2017-09-28 17:18:04 -0400

    Example that draws a square

---
+++ examples/square.rs
@@ -0,0 +1,12 @@
+extern crate turtle;
+
+use turtle::Turtle;
+
+fn main() {
+    let mut turtle = Turtle::new();
+
+    for _ in 0..4 {
+        turtle.forward(200.0);
+        turtle.right(90.0);
+    }
+}
