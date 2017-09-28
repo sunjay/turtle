1b29d326ff5c60102b17e674bdc883833f47b7ee
Sunjay Varma <varma.sunjay@gmail.com>
2017-09-28 18:14:00 -0400

    Added a rotation test example for testing rotation speeds

---
+++ examples/rotationtest.rs
@@ -0,0 +1,48 @@
+extern crate turtle;
+
+use turtle::Turtle;
+
+/// This program demonstrates rotations at multiple speeds
+fn main() {
+    let mut turtle = Turtle::new();
+
+    turtle.pen_up();
+    turtle.set_speed("fastest");
+    turtle.left(90.0);
+    turtle.forward(300.0);
+    turtle.right(180.0);
+    turtle.pen_down();
+
+    for i in 1..12 {
+        turtle.set_speed(i);
+        turtle.right(2.0 * 360.0);
+
+        turtle.pen_up();
+        turtle.set_speed("fastest");
+        turtle.forward(60.0);
+        turtle.pen_down();
+    }
+
+    turtle.hide();
+    turtle.pen_up();
+    turtle.set_speed("fastest");
+    turtle.backward(660.0);
+    turtle.pen_down();
+
+    for i in 1..12 {
+        turtle.set_speed(i);
+        circle(&mut turtle);
+
+        turtle.pen_up();
+        turtle.set_speed("fastest");
+        turtle.forward(60.0);
+        turtle.pen_down();
+    }
+}
+
+fn circle(turtle: &mut Turtle) {
+    for _ in 0..180 {
+        turtle.forward(1.0);
+        turtle.left(2.0);
+    }
+}
