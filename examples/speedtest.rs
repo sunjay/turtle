c52373f9b458101384ec7e215d7a86bf3940e7b0
Sunjay Varma <varma.sunjay@gmail.com>
2017-09-28 17:43:53 -0400

    Added a speed test example to help see the differences between various speeds

---
+++ examples/speedtest.rs
@@ -0,0 +1,31 @@
+extern crate turtle;
+
+use turtle::Turtle;
+
+/// This program draws several parallel lines to demonstrate each of the
+/// different possible movement speeds
+fn main() {
+    let mut turtle = Turtle::new();
+
+    turtle.pen_up();
+    turtle.set_speed("fastest");
+    turtle.left(90.0);
+    turtle.forward(300.0);
+    turtle.right(90.0);
+    turtle.pen_down();
+
+    let length = 200.0;
+
+    for i in 1..12 {
+        turtle.set_speed(i);
+        turtle.forward(length);
+
+        turtle.pen_up();
+        turtle.set_speed("fastest");
+        turtle.backward(length);
+        turtle.right(90.0);
+        turtle.forward(60.0);
+        turtle.left(90.0);
+        turtle.pen_down();
+    }
+}
