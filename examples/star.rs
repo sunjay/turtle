a8541876cf0c3fb7b616972657d20ba238ab23ce
Sunjay Varma <varma.sunjay@gmail.com>
2017-10-01 20:52:51 -0400

    Added an example that draws a star

---
+++ examples/star.rs
@@ -0,0 +1,16 @@
+extern crate turtle;
+
+use turtle::Turtle;
+
+fn main() {
+    let mut turtle = Turtle::new();
+
+    turtle.right(90.0);
+    turtle.set_pen_size(2.0);
+    turtle.set_pen_color("yellow");
+
+    for _ in 0..5 {
+        turtle.forward(300.0);
+        turtle.right(180.0 - (180.0 / 5.0));
+    }
+}
