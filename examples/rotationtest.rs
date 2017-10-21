4c02949cd9b1183578066c9c2dce1757649c9f2f
Sunjay Varma <varma.sunjay@gmail.com>
2017-10-20 21:22:07 -0400

    Added left rotations to the rotation test

--- examples/rotationtest.rs
+++ examples/rotationtest.rs
@@ -22,4 +22,19 @@ fn main() {
         turtle.forward(60.0);
         turtle.pen_down();
     }
+
+    turtle.pen_up();
+    turtle.set_speed("instant");
+    turtle.backward(720.0);
+    turtle.pen_down();
+
+    for i in 1..12 {
+        turtle.set_speed(i);
+        turtle.left(2.0 * 360.0);
+
+        turtle.pen_up();
+        turtle.set_speed("instant");
+        turtle.forward(60.0);
+        turtle.pen_down();
+    }
 }
