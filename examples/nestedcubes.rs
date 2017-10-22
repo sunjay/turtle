dd4bc6e0670da04e6f8ecdf0f2fa31470ca7e763
Sunjay Varma <varma.sunjay@gmail.com>
2017-10-22 01:40:02 -0400

    Added a nested cubes example

---
+++ examples/nestedcubes.rs
@@ -0,0 +1,22 @@
+extern crate turtle;
+
+use turtle::{Turtle, Color};
+
+fn main() {
+    let mut turtle = Turtle::new();
+
+    turtle.set_background_color("light grey");
+    turtle.set_speed(10);
+    turtle.set_pen_size(2.0);
+    for i in 0..290 {
+        turtle.set_pen_color(Color {
+            red: (i as f64 / 300.0 * 4.0) * 255.0 % 255.0,
+            green: 255.0,
+            blue: 255.0,
+            alpha: 1.0,
+        });
+        turtle.forward(i as f64);
+
+        turtle.right(60.0);
+    }
+}
