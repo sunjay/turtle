e174fca9dd23907152641ee5ae58542cfb0d37b2
Sunjay Varma <varma.sunjay@gmail.com>
2017-10-28 22:52:01 -0400

    Added an impl of From<f64> for Speed + tests for all impls of Speed

--- src/speed.rs
+++ src/speed.rs
@@ -107,3 +107,104 @@ impl From<i32> for Speed {
         }
     }
 }
+
+// We include this implementation because we use f64 in the rest of the library.
+// It makes sense to implement this so that they don't get messed up if they accidentally use a
+// floating point literal with set_speed.
+impl From<f64> for Speed {
+    fn from(n: f64) -> Self {
+        (n.round() as i32).into()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use {Turtle};
+
+    #[test]
+    fn speed_names() {
+        let mut turtle = Turtle::new();
+        turtle.set_speed("slowest");
+        assert_eq!(turtle.speed(), Speed::One);
+        turtle.set_speed("slow");
+        assert_eq!(turtle.speed(), Speed::Three);
+        turtle.set_speed("normal");
+        assert_eq!(turtle.speed(), Speed::Six);
+        turtle.set_speed("fast");
+        assert_eq!(turtle.speed(), Speed::Eight);
+        turtle.set_speed("fastest");
+        assert_eq!(turtle.speed(), Speed::Ten);
+        turtle.set_speed("instant");
+        assert_eq!(turtle.speed(), Speed::Instant);
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid speed specified, use one of the words: 'slowest', 'slow', 'normal', 'fast', 'fastest', 'instant'")]
+    fn invalid_speed() {
+        let mut turtle = Turtle::new();
+        turtle.set_speed("wrong");
+    }
+
+    #[test]
+    fn speed_values() {
+        let mut turtle = Turtle::new();
+        turtle.set_speed(0);
+        assert_eq!(turtle.speed(), Speed::Instant);
+        turtle.set_speed(1);
+        assert_eq!(turtle.speed(), Speed::One);
+        turtle.set_speed(2);
+        assert_eq!(turtle.speed(), Speed::Two);
+        turtle.set_speed(3);
+        assert_eq!(turtle.speed(), Speed::Three);
+        turtle.set_speed(4);
+        assert_eq!(turtle.speed(), Speed::Four);
+        turtle.set_speed(5);
+        assert_eq!(turtle.speed(), Speed::Five);
+        turtle.set_speed(6);
+        assert_eq!(turtle.speed(), Speed::Six);
+        turtle.set_speed(7);
+        assert_eq!(turtle.speed(), Speed::Seven);
+        turtle.set_speed(8);
+        assert_eq!(turtle.speed(), Speed::Eight);
+        turtle.set_speed(9);
+        assert_eq!(turtle.speed(), Speed::Nine);
+        turtle.set_speed(10);
+        assert_eq!(turtle.speed(), Speed::Ten);
+        turtle.set_speed(11);
+        assert_eq!(turtle.speed(), Speed::Instant);
+        turtle.set_speed(20394);
+        assert_eq!(turtle.speed(), Speed::Instant);
+    }
+
+    #[test]
+    fn speed_values_f64() {
+        let mut turtle = Turtle::new();
+        turtle.set_speed(0.4);
+        assert_eq!(turtle.speed(), Speed::Instant);
+        turtle.set_speed(1.4);
+        assert_eq!(turtle.speed(), Speed::One);
+        turtle.set_speed(2.4);
+        assert_eq!(turtle.speed(), Speed::Two);
+        turtle.set_speed(3.4);
+        assert_eq!(turtle.speed(), Speed::Three);
+        turtle.set_speed(4.4);
+        assert_eq!(turtle.speed(), Speed::Four);
+        turtle.set_speed(5.4);
+        assert_eq!(turtle.speed(), Speed::Five);
+        turtle.set_speed(6.4);
+        assert_eq!(turtle.speed(), Speed::Six);
+        turtle.set_speed(7.4);
+        assert_eq!(turtle.speed(), Speed::Seven);
+        turtle.set_speed(8.4);
+        assert_eq!(turtle.speed(), Speed::Eight);
+        turtle.set_speed(9.4);
+        assert_eq!(turtle.speed(), Speed::Nine);
+        turtle.set_speed(10.4);
+        assert_eq!(turtle.speed(), Speed::Ten);
+        turtle.set_speed(11.4);
+        assert_eq!(turtle.speed(), Speed::Instant);
+        turtle.set_speed(20394.4);
+        assert_eq!(turtle.speed(), Speed::Instant);
+    }
+}
