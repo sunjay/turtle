952a21db8a69a5e3cfed1e93ebc90c8a1fb81de0
Sunjay Varma <varma.sunjay@gmail.com>
2017-11-22 00:55:55 -0500

    Got the turtle canvas to run the renderer on the main thread

--- src/renderer.rs
+++ src/renderer.rs
@@ -16,30 +16,14 @@ use piston_window::{
     polygon,
 };
 
-use turtle_window::ReadOnly;
+use app::ReadOnly;
 use extensions::ConvertScreenCoordinates;
 use state::{Path, Polygon, Pen, TurtleState, DrawingState};
+use query::DrawingCommand;
 use event::from_piston_event;
-use {Point, Event, Color, color};
-
-pub enum DrawingCommand {
-    /// When a path is finished being animated, it needs to be persisted in the renderer
-    /// so it can be redrawn every frame
-    StorePath(Path),
-    /// Begins filling with the current fill color from the next path onwards. If temporary_path is
-    /// set, it is included in the fill shape. Any paths sent via StorePath will be added to the
-    /// filled shape.
-    /// This command should be passed the fill color that was set at the time when this command
-    /// was issued. We cannot simply poll that from the state when this command is handled because
-    /// that may be well after the fill color has changed.
-    BeginFill(Color),
-    /// Send EndFill to finish the filled shape.
-    EndFill,
-    /// Clears the image completely
-    ///
-    /// Panics if temporary_path is not None
-    Clear,
-}
+use types::Point;
+use event::Event;
+use color::{self, Color};
 
 #[derive(Debug)]
 pub enum Drawing {
