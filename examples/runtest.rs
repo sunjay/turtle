d2c0b836606fe4de61f53cf17b84763b22b82860
Sunjay Varma <varma.sunjay@gmail.com>
2017-11-22 22:04:57 -0500

    Added a runtest to make sure the library can actually run in its normal state

---
+++ examples/runtest.rs
@@ -0,0 +1,17 @@
+//! This is NOT a real example. This is a test designed to see if we can actually run the turtle
+//! process
+
+extern crate turtle;
+
+use std::process;
+
+use turtle::Turtle;
+
+fn main() {
+    let mut turtle = Turtle::new();
+
+    turtle.set_speed(10);
+    turtle.right(90.0);
+    turtle.forward(10.0);
+    process::exit(0);
+}
