48afc4d182ece2299aeabdf23880f15d79dcc1fc
Sunjay Varma <varma.sunjay@gmail.com>
2017-11-23 00:18:07 -0500

    Trying to catch a panic

--- examples/runtest.rs
+++ examples/runtest.rs
@@ -3,15 +3,23 @@
 
 extern crate turtle;
 
+use std::panic;
 use std::process;
 
 use turtle::Turtle;
 
 fn main() {
-    let mut turtle = Turtle::new();
+    panic::catch_unwind(|| {
+        let mut turtle = Turtle::new();
 
-    turtle.set_speed(10);
-    turtle.right(90.0);
-    turtle.forward(10.0);
-    process::exit(0);
+        turtle.set_speed(10);
+        turtle.right(90.0);
+        turtle.forward(10.0);
+        process::exit(0);
+    }).unwrap_or_else(|_| {
+        // Need to force the thread to exit with exit code 1 because the renderer has some
+        // code that will call process::exit(0)
+        eprintln!("panic!");
+        process::exit(1);
+    });
 }
