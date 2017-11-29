8c2ded3eb0982b79fddd75155ab26ab53229f629
Sunjay Varma <varma.sunjay@gmail.com>
2017-11-29 00:35:55 -0500

    JSON messages must now be on their own lines

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,4 @@
-use std::io::{Read, Write};
+use std::io::{Read, Write, BufReader, BufRead};
 use std::sync::mpsc;
 
 use serde_json;
@@ -6,9 +6,19 @@ use serde_json;
 use query::{Query, Response};
 
 /// Read responses from the renderer process and send them back to the TurtleWindow
-pub fn run<R: Read>(mut renderer_stdout: R, response_tx: mpsc::Sender<Response>) {
+pub fn run<R: Read>(renderer_stdout: R, response_tx: mpsc::Sender<Response>) {
+    let mut reader = BufReader::new(renderer_stdout);
     loop {
-        match serde_json::from_reader(&mut renderer_stdout) {
+        let mut buffer = String::new();
+        let read_bytes = reader.read_line(&mut buffer)
+            .expect("bug: unable to read data from renderer process");
+        if read_bytes == 0 {
+            // Reached EOF, renderer process must have quit
+            break;
+        }
+
+        let response: Result<Response, _> = serde_json::from_str(&buffer);
+        match response {
             Ok(response) => match response_tx.send(response) {
                 Ok(_) => {},
                 // The main thread is no longer around, so quit
@@ -28,9 +38,13 @@ pub fn run<R: Read>(mut renderer_stdout: R, response_tx: mpsc::Sender<Response>)
 }
 
 /// Sends a query to the renderer process
-pub fn send_query<W: Write>(renderer_stdin: W, query: &Query) -> Result<(), ()> {
-    match serde_json::to_writer(renderer_stdin, query) {
-        Ok(_) => Ok(()),
+pub fn send_query<W: Write>(mut renderer_stdin: W, query: &Query) -> Result<(), ()> {
+    match serde_json::to_writer(&mut renderer_stdin, query) {
+        Ok(_) => {
+            writeln!(renderer_stdin)
+                .expect("bug: unable to write final newline when sending query");
+            Ok(())
+        },
         Err(err) => {
             if err.is_io() || err.is_eof() {
                 Err(())
