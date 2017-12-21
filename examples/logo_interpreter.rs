908763128aca5fc64c1bf462ae5206bf8a1b0492
Jay Shah <jayshah.9596@gmail.com>
2017-12-21 22:26:49 +0530

    remove code duplication

--- examples/logo_interpreter.rs
+++ examples/logo_interpreter.rs
@@ -14,65 +14,59 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 2 {
-        println!("Interactive LOGO Mode.\nType the commands in stdin.\nPress '^C' (cmd/ctrl+C) to exit");
+        println!(
+            "Interactive LOGO Mode.\nType the commands in stdin.\nPress '^C' (cmd/ctrl+C) to exit"
+        );
         let stdin = io::stdin();
-        let mut reader = BufReader::new(stdin);
-        loop {
-            let mut buffer = String::new();
-            let _ = reader
-                .read_line(&mut buffer)
-                .expect("Unable read input from stdin");
-                let cmd_args = buffer.split_whitespace();
-                handle_command(&mut turtle, cmd_args);
-        }
+        interpret(&mut turtle, stdin)
     }
+    else {
+        let path = &args[1];
+        let file = File::open(path).expect("Could not open provided program");
+        interpret(&mut turtle, file)
+    }
+}
 
-    let path = &args[1];
-    let file = File::open(path).expect("Could not open provided program");
-    let mut reader = BufReader::new(file);
+fn interpret<R: io::Read>(turtle: &mut Turtle, input: R) {
+    let mut reader = BufReader::new(input);
     loop {
         let mut buffer = String::new();
-        let read_bytes = reader
-            .read_line(&mut buffer)
-            .expect("Unable read input file");
+        let read_bytes = reader.read_line(&mut buffer)
+            .expect("Unable read input");
         if read_bytes == 0 {
-            // Reached EOF, stop interpreter
+            // Reached EOF, break loop
             break;
         }
-
         let cmd_args = buffer.split_whitespace();
-        handle_command(&mut turtle, cmd_args);
+        handle_command(turtle, cmd_args);
     }
 }
 
 fn handle_command<'a, A: Iterator<Item = &'a str>>(turtle: &mut Turtle, mut args: A) {
     while let Some(command) = args.next() {
-        match command {
+        match command
+        {
             "fd" | "forward" => {
-                let distance = parse_distance(
-                    args.next()
-                        .expect("Expected a distance value after fd/forward command"),
+                let distance = parse_distance(args.next()
+                    .expect("Expected a distance value after fd/forward command")
                 );
                 turtle.forward(distance);
             },
             "bk" | "back" => {
-                let distance = parse_distance(
-                    args.next()
-                        .expect("Expect a distance value after bk/back command"),
+                let distance = parse_distance(args.next()
+                    .expect("Expect a distance value after bk/back command")
                 );
                 turtle.backward(distance);
             },
             "lt" | "left" => {
-                let distance = parse_distance(
-                    args.next()
-                        .expect("Expect a distance value after lt/left command"),
+                let distance = parse_distance(args.next()
+                    .expect("Expect a distance value after lt/left command")
                 );
                 turtle.left(distance);
             },
             "rt" | "right" => {
-                let distance = parse_distance(
-                    args.next()
-                        .expect("Expect a distance value after rt/right command"),
+                let distance = parse_distance(args.next()
+                    .expect("Expect a distance value after rt/right command")
                 );
                 turtle.right(distance);
             },
@@ -82,6 +76,6 @@ fn handle_command<'a, A: Iterator<Item = &'a str>>(turtle: &mut Turtle, mut args
 }
 
 fn parse_distance(s: &str) -> f64 {
-    let dist: f64 = s.parse().expect("Not a valide number");
+    let dist: f64 = s.parse().expect("Not a valid number");
     dist
 }
