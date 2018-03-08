ad5e3298549d3fc339acc5f4a796ff390e53048e
Sunjay Varma <varma.sunjay@gmail.com>
2018-03-08 18:40:08 -0500

    Implementing and testing large parts of Speed type, fixing some examples and rewriting docs

--- src/speed.rs
+++ src/speed.rs
@@ -1,33 +1,151 @@
 use std::fmt;
 use std::f64;
 use std::f64::consts::PI;
+use std::cmp::Ordering;
 
 use radians::Radians;
 use rand::{Rand, Rng};
 use {Distance};
 
+const MIN_SPEED: i32 = 1;
 const MAX_SPEED: i32 = 25;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Ord, Hash, Serialize, Deserialize)]
 pub(crate) enum SpeedLevel {
     Value(i32),
     Instant,
 }
 
+impl PartialOrd for SpeedLevel {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        use self::SpeedLevel::*;
+        match (*self, *other) {
+            (Value(value), Value(ref other_value)) => value.partial_cmp(other_value),
+            (Instant, Instant) => Some(Ordering::Equal),
+            (Value(_), Instant) => Some(Ordering::Less),
+            (Instant, Value(_)) => Some(Ordering::Greater),
+        }
+    }
+}
+
 /// Represents both the movement and rotation speed of the turtle.
 ///
-/// You can create a `Speed` value by converting it from a number of different types. `Speed`
-/// values can be compared for equality with `i32` values. This is a little more convenient than
-/// converting the `i32` to `Speed` every time you want to make a comparison.
+/// # Creating Speeds
+///
+/// You can create a `Speed` value by converting either strings or numbers.
 ///
 /// ```rust
 /// # use turtle::Speed;
 /// // This value is of type `Speed` and it is converted from an `i32`
 /// let speed: Speed = 1.into();
+///
+/// // This value is of type `Speed` and it is converted from a `&str`
+/// let slowest_speed: Speed = "slowest".into();
+/// assert_eq!(speed, slowest_speed);
+/// ```
+///
+/// There is no need to call `.into()` when passing a speed into the [`set_speed` method].
+/// See the [`set_speed` method] for more information about how that works.
+///
+/// ```rust
+/// # use turtle::{Turtle};
+/// let mut turtle = Turtle::new();
+/// turtle.set_speed(22); // Same as `turtle.set_speed(22.into())`
+/// ```
+///
+/// **The minimum speed value is 1 and the maximum speed value (currently) is 25.**
+///
+/// Trying to set the speed to a value out of that range will cause a panic.
+///
+/// ```rust,should_panic
+/// # use turtle::{Turtle};
+/// let mut turtle = Turtle::new();
+/// turtle.set_speed(26); // panic!
+/// ```
+///
+/// While the minimum speed will not change, the maximum speed may grow larger if the need arises.
+/// That is why we chose to panic for invalid speeds instead of defaulting to another value.
+///
+/// ### String Conversion
+///
+/// Strings are converted as follows:
+///
+/// | String      | Value |
+/// | ----------- | ----- |
+/// | `"slowest"`   | `1`   |
+/// | `"slower"`    | `5`   |
+/// | `"slow"`      | `8`   |
+/// | `"normal"`    | `10`  |
+/// | `"fast"`      | `12`  |
+/// | `"faster"`    | `15`  |
+/// | `"instant"`   | [see below](#instant) |
+///
+/// You can use strings to create `Speed` values in the same way numbers were used above. All
+/// three of the following are equivalent:
+///
+/// ```rust
+/// # use turtle::{Turtle, Speed};
+/// # let mut turtle = Turtle::new();
+/// turtle.set_speed(5);
+/// turtle.set_speed("slower");
+/// turtle.set_speed(Speed::from("slower")); // Not recommended!
+/// ```
+///
+/// # Instant
+///
+/// There is one special speed value `"instant"` which makes it so that movement and rotation
+/// are not animated at all. Instead, the turtle immediately moves and rotates to the position that
+/// you directed it to. It will still draw along the way if its pen is down.
+///
+/// ```rust
+/// # use turtle::{Turtle};
+/// let mut turtle = Turtle::new();
+/// turtle.set_speed("instant");
+/// turtle.forward(100.0); // A line will be drawn instantly!
+/// ```
+///
+/// # Comparing Speed Values
+///
+/// `Speed` values can be compared for equality with `i32` values. This is a little more convenient
+/// than converting the `i32` to `Speed` every time you want to make a comparison.
+///
+/// ```rust
+/// # use turtle::Speed;
+/// let speed: Speed = 1.into();
 /// // Speed values can be compared to integers
 /// assert_eq!(speed, 1);
 /// // This is equivalent to the following
 /// assert_eq!(speed, Speed::from(1));
+///
+/// // This value is of type `Speed` and it is converted from a `&str`
+/// let speed: Speed = "slowest".into();
+/// // Speed values can be compared to integers
+/// assert_eq!(speed, 1);
+/// // This is equivalent to the following
+/// assert_eq!(speed, Speed::from("slowest"));
+/// ```
+///
+/// ```rust
+/// # use turtle::Speed;
+/// let speed: Speed = 12.into();
+/// if speed == 12 && speed >= 5 && speed < Speed::instant() {
+///     println!("Super fast!!");
+/// }
+/// # else { panic!("Unable to compare a speed value!"); }
+/// // This is equivalent, but requires more typing
+/// if speed == Speed::from(12) && speed >= Speed::from(5) && speed < Speed::from("instant") {
+///     println!("Super fast!!");
+/// }
+/// ```
+///
+/// Notice that you can compare `Speed` values to numeric values, not the other way around.
+///
+/// ```rust,compile_fail
+/// # use turtle::Speed;
+/// let speed: Speed = 7.into();
+/// if 8 > speed { // This doesn't make sense and won't compile!
+///     // ...
+/// }
 /// ```
 ///
 /// To check if a speed is instant, use the [`is_instant()` method] or compare the speed to
@@ -37,34 +155,36 @@ pub(crate) enum SpeedLevel {
 /// # use turtle::Speed;
 /// let speed = Speed::instant();
 /// if speed.is_instant() {
-///     println!("Super fast!!");
+///     println!("Instant!!");
 /// }
+/// # else { panic!("Speed::instant() was not instant!"); }
 /// ```
-///
-/// There is no need to call `.into()` when passing a speed into the [`set_speed` method].
-///
-/// ```rust
-/// # use turtle::{Turtle};
-/// let mut turtle = Turtle::new();
-/// turtle.set_speed(22);
-/// ```
-///
-/// See the [`set_speed` method] for more information.
 ///
 /// [`set_speed` method]: struct.Turtle.html#method.set_speed
 /// [`Speed::instant()`]: struct.Speed.html#method.instant
 /// [`is_instant()` method]: struct.Speed.html#method.is_instant
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
 pub struct Speed(SpeedLevel);
 
 impl Speed {
-    /// Returns the speed value that represents instantly moving and rotating with no further
-    /// animation
+    /// Returns the speed value that will make the turtle move and rotate instantly. This means
+    /// that instead of the turtle's movements being animated, it will directly move to wherever
+    /// you direct it to go.
     ///
     /// ```rust
-    /// use turtle::{Speed};
-    /// let speed = Speed::instant();
-    /// assert!(speed.is_instant());
+    /// # use turtle::{Turtle};
+    /// let mut turtle = Turtle::new();
+    /// turtle.set_speed(Speed::instant());
+    /// turtle.forward(100.0); // A line will be drawn instantly!
+    /// ```
+    ///
+    /// Whenever possible, you should prefer to use the string `"instant"` instead of calling this
+    /// method.
+    ///
+    /// ```rust
+    /// # use turtle::{Turtle};
+    /// # let mut turtle = Turtle::new();
+    /// turtle.set_speed("instant"); // equivalent to typing `Speed::instant()`
     /// ```
     pub fn instant() -> Self {
         Speed(SpeedLevel::Instant)
@@ -75,6 +195,9 @@ impl Speed {
     /// ```rust
     /// use turtle::{Speed};
     /// let speed: Speed = "instant".into();
+    /// assert!(speed.is_instant());
+    ///
+    /// let speed = Speed::instant();
     /// assert!(speed.is_instant());
     /// ```
     pub fn is_instant(self) -> bool {
@@ -113,6 +236,18 @@ impl fmt::Display for Speed {
     }
 }
 
+impl PartialEq<i32> for Speed {
+    fn eq(&self, other: &i32) -> bool {
+        self.eq(&Speed::from(*other))
+    }
+}
+
+impl PartialOrd<i32> for Speed {
+    fn partial_cmp(&self, other: &i32) -> Option<Ordering> {
+        return self.partial_cmp(&Speed::from(*other));
+    }
+}
+
 impl Rand for Speed {
     fn rand<R: Rng>(rng: &mut R) -> Self {
         (rng.gen::<i32>() % MAX_SPEED).into()
@@ -136,19 +271,14 @@ impl<'a> From<&'a str> for Speed {
     }
 }
 
-impl PartialEq<i32> for Speed {
-    fn eq(&self, other: &i32) -> bool {
-        self.eq(&Speed::from(*other))
-    }
-}
-
 impl From<i32> for Speed {
     fn from(n: i32) -> Self {
         use self::SpeedLevel::*;
 
         Speed(match n {
+            // Special error message for 0 because this used to be a valid speed
             0 => panic!("Invalid speed: 0. If you wanted to set the speed to instant, please use the string \"instant\" or Speed::instant()"),
-            n if n <= MAX_SPEED => Value(n),
+            n if n >= MIN_SPEED && n <= MAX_SPEED => Value(n),
             n => panic!("Invalid speed: {}. Must be a value between 1 and {}", n, MAX_SPEED),
         })
     }
@@ -225,6 +355,20 @@ mod tests {
     fn speed_value_out_of_range2() {
         let mut turtle = Turtle::new();
         turtle.set_speed(20394);
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid speed: -1. Must be a value between 1 and 25")]
+    fn speed_value_out_of_range_negative() {
+        let mut turtle = Turtle::new();
+        turtle.set_speed(-1);
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid speed: 0. If you wanted to set the speed to instant, please use the string \"instant\" or Speed::instant()")]
+    fn disallow_zero() {
+        let mut turtle = Turtle::new();
+        turtle.set_speed(0);
     }
 
     #[test]
