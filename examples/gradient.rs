14e7ce3facb608f6988fcd703d2a6824342a1d77
Sunjay Varma <varma.sunjay@gmail.com>
2018-04-17 16:46:52 -0400

    Creating a gradient using color mixing

---
+++ examples/gradient.rs
@@ -0,0 +1,23 @@
+//! This example shows how you can use color mixing and pen size to produce a smooth gradient
+
+extern crate turtle;
+
+use turtle::{Color, Turtle};
+
+fn main() {
+    let mut turtle = Turtle::new();
+    turtle.drawing_mut().set_title("Gradients!");
+    turtle.set_pen_size(200.0);
+
+    turtle.pen_up();
+    turtle.backward(250.0);
+    turtle.pen_down();
+
+    let red: Color = "red".into();
+    let white: Color = "white".into();
+
+    for i in 0..100+1 {
+        turtle.set_pen_color(red.mix(white, i));
+        turtle.forward(5.0);
+    }
+}
