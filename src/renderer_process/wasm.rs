e2cfd2893263e7c1f01f2950d8a50b92479436a5
Sunjay Varma <varma.sunjay@gmail.com>
2018-07-09 17:14:04 -0700

    Successfully reading a string in JavaScript from Rust WASM

--- src/renderer_process/wasm.rs
+++ src/renderer_process/wasm.rs
@@ -1,23 +1,65 @@
 #[cfg(not(target_arch = "wasm32"))]
 compile_error!("This module should only be included when compiling to wasm");
 
+use std::mem;
+use std::ffi::CString;
+use std::os::raw::{c_char, c_void};
+
+use serde_json;
+
 use query::{Query, Response};
+
+// Functions preovided by JavaScript, to be called by the WebAssembly generated from Rust
+extern "C" {
+    fn send_query(query: *const c_char);
+}
+
+// In order to work with the memory in WASM, we expose allocation and deallocation methods
+#[no_mangle]
+pub extern "C" fn alloc(size: usize) -> *const c_void {
+    let buf = Vec::with_capacity(size);
+    let ptr = buf.as_ptr();
+    // Forget the pointer so that Rust doesn't free the memory we want to give JavaScript
+    // access to. (Leaking memory is **safe**, so unsafe { ... } is not necessary.)
+    mem::forget(buf);
+    return ptr as *const c_void;
+}
+
+#[no_mangle]
+pub extern "C" fn dealloc(ptr: *mut c_void, cap: usize) {
+    unsafe {
+        // Rust will drop this vector and free the memory
+        let _buf = Vec::from_raw_parts(ptr, 0, cap);
+    }
+}
+
+/// Specifically for deallocating NULL-terminated strings without knowing their length in advance
+///
+/// See `CString::into_raw` for more information:
+/// https://doc.rust-lang.org/std/ffi/struct.CString.html#method.into_raw
+#[no_mangle]
+pub extern "C" fn dealloc_str(ptr: *mut c_char) {
+    unsafe {
+        let _ = CString::from_raw(ptr);
+    }
+}
 
 /// A special "renderer process" specifically for communicating through the web assembly boundary
 /// to the JavaScript that is running this program.
-#[cfg(all(target_arch = "wasm32", not(any(feature = "test", test))))]
-pub struct RendererProcess {
-}
+pub struct RendererProcess {}
 
-#[cfg(all(target_arch = "wasm32", not(any(feature = "test", test))))]
 impl RendererProcess {
     pub fn new() -> Self {
-        Self {
-        }
+        Self {}
     }
 
     pub fn send_query(&mut self, query: Query) -> Option<Response> {
-        println!("{}", ::serde_json::to_string(&query).unwrap());
-        None
+        let query_str = serde_json::to_string(&query).unwrap();
+        let c_str = CString::new(query_str).unwrap();
+        let raw_str = c_str.into_raw();
+        // Once the string is passed into JavaScript, JavaScript is now considered the owner of
+        // that string.
+        unsafe { send_query(raw_str) };
+        None //TODO
     }
 }
