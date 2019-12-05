cfe41cd7185568d4e9c0e80736448b692c25aba3
myrrlyn <self@myrrlyn.dev>
2019-12-05 14:47:10 -0700

    Add some more comments

--- examples/bits.rs
+++ examples/bits.rs
@@ -3,15 +3,23 @@
 //! This example uses [@myrrlyn]’s [`bitvec`] crate to turn data into strings of
 //! bits, and then draws them on the screen.
 //!
+//! You are encouraged to change both the data used to seed the turtle, and the
+//! `bitvec` calls that control how the turtle acts, to see what changes.
+//!
 //! [@myrrlyn]: //github.com/myrrlyn
 //! [`bitvec`]: //crates.io/crates/bitvec
 
+// The `bitvec` crate provides a bunch of types for us to use with one import
 use bitvec::prelude::*;
 use turtle::Turtle;
 
+/// Set the line length for each displayed bit
 const BIT_WIDTH: f64 = 20.0;
+/// Set the vertical spacing between successive rows of bits
 const BIT_HEIGHT: f64 = 10.0;
+/// Set the horizontal spacing between successive bits in a row
 const BIT_MARGIN: f64 = BIT_WIDTH / 2.0;
+/// Compute the total width of a bit plus its spacing
 const BIT_BOX: f64 = BIT_WIDTH + BIT_MARGIN;
 
 fn main() {
@@ -24,10 +32,13 @@ fn main() {
     // then backtracking and walking vertically to the next byte.
     let mut turtle = Turtle::new();
     turtle.pen_up();
+    // Compute the boundaries of the part of the screen where the turtle will
+    // draw
     let right_edge = BIT_BOX * 4.0;
     let top_edge = BIT_HEIGHT * (text.len() as f64 / 2.0);
-    // The turtle starts from the top right of the region, and moves left
+    // The turtle starts from the top right of the region,
     turtle.go_to((right_edge, top_edge));
+    // and walks left
     turtle.set_heading(180.0);
 
     // Now let’s draw some data on the screen, as individual bits
@@ -60,21 +71,23 @@ fn main() {
     let bits = raw_number.bits::<BigEndian>();
 
     // Since we are reading bits from left to right, the turtle should move from
-    // left to right also.
-    let left_edge = -right_edge;
-    turtle.set_x(left_edge * 2.0);
+    // left to right also. Change the `* 2.0` to move the section horizontally.
+    let left_edge = -right_edge * 2.0;
+    turtle.set_x(left_edge);
+    // Walk from left to right
     turtle.set_heading(0.0);
 
     // The `&BitSlice` type acts just like `&[bool]`, so it comes with a
     // `.chunks` method which divides it into smaller pieces. `bitvec` can take
     // any number, not just multiples of 8, but 16 is a convenient number to
-    // look at.
+    // look at. Try changing it to a different number, like 10, to see what
+    // happens!
     for row in bits.chunks(16) {
         // Each chunk produced is a smaller `&BitSlice`, just like
         // `&[bool].chunks` produces smaller `&[bool]`s, so we can draw it.
         draw_row(&mut turtle, row);
         // After each row, the turtle has to go back to the left edge.
-        turtle.set_x(left_edge * 2.0);
+        turtle.set_x(left_edge);
     }
 }
 
@@ -96,8 +109,9 @@ where C: Cursor, T: BitStore {
             t.set_pen_color("light grey");
         }
 
-        // For each bit, the loop puts down the pen to draw a line of the bit
-        // color, then picks up the pen to add some spacing between them
+        // For each bit, the loop puts down the pen to draw a line of the bit’s
+        // color, then picks up the pen to add some horizontal spacing between
+        // them.
         t.pen_down();
         t.forward(BIT_WIDTH);
         t.pen_up();
@@ -114,5 +128,5 @@ where C: Cursor, T: BitStore {
     t.forward(BIT_HEIGHT);
     // then goes back to its old direction.
     t.set_heading(old_heading);
-    // This way each row gets spacing between them.
+    // This way each row gets vertical spacing between them.
 }
