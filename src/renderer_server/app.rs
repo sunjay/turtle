487196a6677d2def2bc1fb2067a598dc3d60377f
Sunjay Varma <varma.sunjay@gmail.com>
2020-04-30 21:19:55 -0400

    Solved problem with nested locking by wrapping inner TurtleDrawings in an Arc

--- src/renderer_server/app.rs
+++ src/renderer_server/app.rs
@@ -1,10 +1,10 @@
+use std::sync::Arc;
+
 use serde::{Serialize, Deserialize};
-use tokio::sync::{RwLock, Mutex};
+use tokio::sync::{RwLock, Mutex, MutexGuard};
 
 use super::state::{TurtleState, DrawingState};
-use super::renderer::display_list::PrimHandle;
-
-//TODO: pub type TurtleDrawingsGuard<'a> = MappedRwLockReadGuard<'a, MutexGuard<'a, TurtleDrawings>>;
+use super::renderer::display_list::{DisplayList, PrimHandle};
 
 /// The unique ID of a particular turtle
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
@@ -16,19 +16,21 @@ pub struct TurtleDrawings {
     pub drawings: Vec<PrimHandle>,
 }
 
-/// The entire state of the application
+/// The entire state of the application, shared between threads in the server
 #[derive(Default, Debug)]
 pub struct App {
+    /// All of the drawing primitives in the order in which they wil be drawn
+    display_list: Mutex<DisplayList>,
     /// The current state of the drawing
     drawing: Mutex<DrawingState>,
     /// Each `TurtleId` indexes into this field
     ///
     /// Note that we have to be very careful deleting from this field since we don't want to
-    /// invalidate any `TurtleId`.
+    /// invalidate any `TurtleId` or cloned `Arc<Mutex<TurtleDrawings>>`.
     ///
-    /// The outer `RwLock` makes it possible to push into the `Vec` using `write` and also lock
+    /// The outer `RwLock` makes it possible to push into the `Vec` using `write` and also `clone`
     /// multiple items in the `Vec` at the same time using `read`.
-    turtles: RwLock<Vec<Mutex<TurtleDrawings>>>,
+    turtles: RwLock<Vec<Arc<Mutex<TurtleDrawings>>>>,
 }
 
 impl App {
@@ -40,10 +42,20 @@ impl App {
         id
     }
 
-    //TODO: Wait for https://github.com/tokio-rs/tokio/issues/2471
-    ///// Returns a mutable handle to a the state and drawings of the given turtle
-    //pub async fn turtle_mut(&self, id: TurtleId) -> TurtleDrawingsGuard {
-    //    let TurtleId(index) = id;
-    //    RwLockReadGuard::map(self.turtles.read().await, |turtles| turtles[index].lock()).await
-    //}
+    /// Returns a mutable handle to the display list
+    pub async fn display_list_mut(&self) -> MutexGuard<'_, DisplayList> {
+        self.display_list.lock().await
+    }
+
+    /// Returns a mutable handle to the drawing state
+    pub async fn drawing_mut(&self) -> MutexGuard<'_, DrawingState> {
+        self.drawing.lock().await
+    }
+
+    /// Returns a handle to a the state and drawings of the given turtle
+    pub async fn turtle(&self, id: TurtleId) -> Arc<Mutex<TurtleDrawings>> {
+        let TurtleId(index) = id;
+        let turtles = self.turtles.read().await;
+        turtles[index].clone()
+    }
 }
