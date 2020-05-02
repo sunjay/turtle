0fff47a825a9bea547a47c1789b31d411f329763
Sunjay Varma <varma.sunjay@gmail.com>
2020-05-02 18:24:28 -0400

    Revert "Started to work on using Slab, but it didn't really work out for similar reasons to RwLock<Vec<Mutex>>. Slab also has a guard type that prevents the exact interface we want."
    This reverts commit 660701de58f3b301b96e67f9da92897668a49a7c.

--- src/renderer_server/app.rs
+++ src/renderer_server/app.rs
@@ -1,14 +1,10 @@
 use std::sync::Arc;
 
 use serde::{Serialize, Deserialize};
-use tokio::sync::{Mutex, MutexGuard};
-use sharded_slab::Slab;
+use tokio::sync::{RwLock, Mutex, MutexGuard};
 
 use super::state::{TurtleState, DrawingState};
 use super::renderer::display_list::PrimHandle;
-
-/// Slab uses `usize` for its ID type
-type SlabId = usize;
 
 /// The unique ID of a particular turtle
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
@@ -25,28 +21,22 @@ pub struct TurtleDrawings {
 pub struct App {
     /// The current state of the drawing
     drawing: Mutex<DrawingState>,
-    /// The data for each turtle
+    /// Each `TurtleId` indexes into this field
     ///
-    /// This field could be replaced with an append-only lock-free list or even an
-    /// `RwLock<Vec<Mutex<T>>>` if it were possible to hold both the read guard and the inner mutex
-    /// guard at the same time in the same struct. The main requirements here are that you can
-    /// access multiple turtles concurrently (so it must be Sync) and that you can push new turtles
-    /// in as needed. So lock-freedom isn't strictly necessary (see RwLock<Vec<Mutex<T>>>), but it
-    /// is nice to have because it works well in an async-context (no blocking). The `appendlist`
-    /// crate fits these requirements, but it is not Sync, so it can't be used here.
-    turtles: Slab<Mutex<TurtleDrawings>>,
-    /// Each `TurtleId` indexes into this field to get the ID used to access the slab
-    turtle_ids: Mutex<Vec<SlabId>>,
+    /// Note that we have to be very careful deleting from this field since we don't want to
+    /// invalidate any `TurtleId` or cloned `Arc<Mutex<TurtleDrawings>>`.
+    ///
+    /// The outer `RwLock` makes it possible to push into the `Vec` using `write` and also `clone`
+    /// multiple items in the `Vec` at the same time using `read`.
+    turtles: RwLock<Vec<Arc<Mutex<TurtleDrawings>>>>,
 }
 
 impl App {
     /// Adds a new turtle to the application state, returning its `TurtleId`
     pub async fn add_turtle(&self) -> TurtleId {
-        let slab_ids = self.turtle_ids.lock().await;
-        let id = TurtleId(slab_ids.len());
-        let slab_id = self.turtles.insert(Default::default())
-            .expect("bug: unable to create turtle, shard is full");
-        slab_ids.push(slab_id);
+        let mut turtles = self.turtles.write().await;
+        let id = TurtleId(turtles.len());
+        turtles.push(Default::default());
         id
     }
 
@@ -58,7 +48,6 @@ impl App {
     /// Returns a handle to a the state and drawings of the given turtle
     pub async fn turtle(&self, id: TurtleId) -> Arc<Mutex<TurtleDrawings>> {
         let TurtleId(index) = id;
-        let slab_id = self.turtle_ids.lock()[index];
         let turtles = self.turtles.read().await;
         turtles[index].clone()
     }
