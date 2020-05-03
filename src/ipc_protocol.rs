afe4ca9b32b556c2bcd2b61f04721425b855fe57
Sunjay Varma <varma.sunjay@gmail.com>
2020-05-03 14:32:12 -0400

    Moving server connection code to ipc protocol

--- src/ipc_protocol.rs
+++ src/ipc_protocol.rs
@@ -90,6 +90,26 @@ pub struct ServerConnection {
 }
 
 impl ServerConnection {
+    /// Establishes a connection to the client by reading from stdin
+    pub async fn connect_stdin() -> Result<Self, ConnectionError> {
+        use tokio::io::{self, AsyncBufReadExt};
+
+        let stdin = io::stdin();
+        let mut reader = io::BufReader::new(stdin);
+
+        let mut oneshot_name = String::new();
+        reader.read_line(&mut oneshot_name).await?;
+        if oneshot_name.is_empty() {
+            panic!("bug: unexpected EOF when reading oneshot server name");
+        }
+
+        // Remove the trailing newline
+        assert_eq!(oneshot_name.pop(), Some('\n'));
+        let conn = ServerConnection::connect(oneshot_name)?;
+
+        Ok(conn)
+    }
+
     /// Establishes a connection with the IPC channel oneshot server with the given name
     pub fn connect(oneshot_name: String) -> Result<Self, ConnectionError> {
         let (server_sender, receiver) = ipc::channel()?;
