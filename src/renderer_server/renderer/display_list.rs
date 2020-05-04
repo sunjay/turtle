cca56af167d7033d84b56c14673ec353d824ce44
Sunjay Varma <varma.sunjay@gmail.com>
2020-05-04 14:24:52 -0400

    Added APIs to push into and update fill polygon, used that in animation

--- src/renderer_server/renderer/display_list.rs
+++ src/renderer_server/renderer/display_list.rs
@@ -32,6 +32,22 @@ impl DisplayList {
         todo!()
     }
 
+    /// Pushes a point into a polygon with the given handle
+    ///
+    /// Returns the index of that point in the polygon (guaranteed to be valid because points
+    /// cannot be removed from polygons).
+    pub fn polygon_push(&mut self, handle: PrimHandle, point: Point) -> usize {
+        todo!()
+    }
+
+    /// Modifies a point in a polygon at the given index to be the given point
+    ///
+    /// Note that the provided index should only ever be one that was previously returned from
+    /// the `polygon_push` method.
+    pub fn polygon_update(&mut self, handle: PrimHandle, index: usize, point: Point) {
+        todo!()
+    }
+
     /// Removes the given items from the display list
     pub fn remove<I: Iterator<Item=PrimHandle>>(&mut self, items: I) {
         todo!()
