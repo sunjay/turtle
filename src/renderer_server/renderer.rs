41325f7b4f19587654aa60daaece3c17b4bab18b
Sunjay Varma <varma.sunjay@gmail.com>
2020-05-05 14:43:02 -0400

    Defining conversion between logical/world coordinates (Point) to screen coordinates (Vector2F) + tests to verify that this works

--- src/renderer_server/renderer.rs
+++ src/renderer_server/renderer.rs
@@ -4,7 +4,7 @@ pub mod export;
 use glutin::dpi::PhysicalSize;
 use pathfinder_canvas::{Canvas, CanvasFontContext, Path2D};
 use pathfinder_color::ColorF;
-use pathfinder_geometry::vector::{vec2f, vec2i};
+use pathfinder_geometry::vector::{vec2f, vec2i, Vector2F};
 use pathfinder_gl::{GLDevice, GLVersion};
 use pathfinder_resources::embedded::EmbeddedResourceLoader;
 use pathfinder_renderer::{
@@ -29,6 +29,37 @@ fn convert_color(color: Color) -> ColorF {
         (green / RGB_MAX_VAL) as f32,
         (blue / RGB_MAX_VAL) as f32,
         alpha as f32,
+    )
+}
+
+/// Converts a `Point` in logical or "world" coordinates to a `Vector2F` in screen coordinates
+///
+/// # Parameters
+///
+/// * `point` is the `Point` to convert to screen coordinates
+/// * `dpi_scale` is the high DPI scale factor (>= 0.0)
+/// * `center` is the `Point` configured in the drawing that all other `Point`s are relative to.
+/// * `fb_center` is the center of the framebuffer in screen coordinates.
+///
+/// # Coordinate Systems
+///
+/// * logical or "world" coordinates (cartesian coordinates)
+///   * origin is in the center of the framebuffer and can be offset by `center`
+///   * x is left to right
+///   * y is bottom to top
+/// * screen coordinates
+///   * origin is the top-left corner of the framebuffer
+///   * x is left to right
+///   * y is top to bottom
+fn to_screen_coords(point: Point, dpi_scale: f64, center: Point, fb_center: Vector2F) -> Vector2F {
+    let Point {x, y} = point;
+    let Point {x: center_x, y: center_y} = center;
+    let fb_center_x = fb_center.x();
+    let fb_center_y = fb_center.y();
+
+    vec2f(
+        ((x - center_x) * dpi_scale) as f32 + fb_center_x,
+        -((y - center_y) * dpi_scale) as f32 + fb_center_y,
     )
 }
 
@@ -84,5 +115,39 @@ impl Renderer {
         // Build and render scene
         self.scene.replace_scene(canvas.into_canvas().into_scene());
         self.scene.build_and_render(&mut self.renderer, BuildOptions::default());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn screen_coords() {
+        // The origin is always at fb_center as long as center is also the origin
+        let screen_coord = to_screen_coords(Point::origin(), 1.0, Point::origin(), vec2f(200.0, 300.0));
+        assert_eq!(screen_coord, vec2f(200.0, 300.0));
+        // The origin is always at fb_center regardless of DPI scale
+        let screen_coord = to_screen_coords(Point::origin(), 2.0, Point::origin(), vec2f(200.0, 300.0));
+        assert_eq!(screen_coord, vec2f(200.0, 300.0));
+
+        // The x-axis and y-axis treated distinctly and interpreted as cartesian
+        let screen_coord = to_screen_coords(Point {x: 10.0, y: 20.0}, 1.0, Point::origin(), vec2f(200.0, 300.0));
+        assert_eq!(screen_coord, vec2f(210.0, 280.0));
+        // A different fb_center gives a different final coordinate
+        let screen_coord = to_screen_coords(Point {x: 10.0, y: 20.0}, 1.0, Point::origin(), vec2f(300.0, 400.0));
+        assert_eq!(screen_coord, vec2f(310.0, 380.0));
+
+        // The center is interpreted as cartesian and points are relative to it
+        let screen_coord = to_screen_coords(Point {x: 10.0, y: 20.0}, 1.0, Point {x: 30.0, y: 5.0}, vec2f(200.0, 300.0));
+        assert_eq!(screen_coord, vec2f(180.0, 285.0));
+
+        // Negative points work too
+        let screen_coord = to_screen_coords(Point {x: -10.0, y: -20.0}, 1.0, Point {x: 30.0, y: -5.0}, vec2f(200.0, 300.0));
+        assert_eq!(screen_coord, vec2f(160.0, 315.0));
+
+        // DPI scale > 1.0 causes logical coordinates to scale, but NOT screen coordinates
+        let screen_coord = to_screen_coords(Point {x: 10.0, y: 20.0}, 2.0, Point {x: 30.0, y: 5.0}, vec2f(200.0, 300.0));
+        assert_eq!(screen_coord, vec2f(160.0, 270.0));
     }
 }
