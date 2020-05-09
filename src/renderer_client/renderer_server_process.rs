7e5264c28a6d64b643d0555c618c5ba96bde45e1
Sunjay Varma <varma.sunjay@gmail.com>
2020-05-09 18:12:54 -0400

    Re-writing child process finalization logic

--- src/renderer_client/renderer_server_process.rs
+++ src/renderer_client/renderer_server_process.rs
@@ -1,6 +1,6 @@
 use std::io;
 use std::env;
-use std::process::{Stdio, exit};
+use std::process::{self, Stdio, ExitStatus};
 
 use tokio::{
     runtime,
@@ -14,16 +14,24 @@ pub const RENDERER_PROCESS_ENV_VAR: &str = "RUN_TURTLE_CANVAS";
 
 #[derive(Debug)]
 pub struct RendererServerProcess {
-    task_handle: Option<JoinHandle<()>>,
+    /// A handle to the runtime that the process was spawned in. This is needed because a handle
+    /// to the runtime can only be created when a "runtime context". Since Drop may not always run
+    /// from async code, we need this to ensure we can wait on the subprocess in `task_handle`.
+    /// NOTE: This creates an implicit invariant that this struct must be dropped before the
+    /// runtime that it was created in is dropped. This is not an issue in normal code and will at
+    /// worst cause a panic!().
+    runtime_handle: runtime::Handle,
+    /// A handle to the running task. This can be waited on to find out if the process exited
+    /// successfully
+    task_handle: Option<JoinHandle<io::Result<ExitStatus>>>,
     /// A handle to the stdin of the child process
     child_stdin: ChildStdin,
 }
 
 impl RendererServerProcess {
     /// Spawn a new process for the renderer
-    pub async fn spawn() -> Self {
-        let current_exe = env::current_exe()
-            .expect("Could not read path of the currently running executable");
+    pub async fn spawn() -> io::Result<Self> {
+        let current_exe = env::current_exe()?;
 
         // The new process is the same executable as this process but with a special environment
         // variable passed in
@@ -33,30 +41,20 @@ impl RendererServerProcess {
             //
             // stdout/stderr will be inherited from the current process
             .stdin(Stdio::piped())
-            .spawn()
-            .expect("failed to start separate process for renderer");
+            .kill_on_drop(true)
+            .spawn()?;
 
         let child_stdin = child.stdin.take()
             .expect("renderer process was not spawned with a handle to stdin");
 
-        // Ensure the child process is spawned in the runtime so it can
-        // make progress on its own while we send it input.
-        let task_handle = Some(tokio::spawn(async {
-            // We want to await in a separate task because otherwise the current task would not
-            // make any progress until the child process is complete
-            let status = child.await
-                .expect("child process encountered an error");
+        // Spawn a separate task for the child process so this task can continue to make progress
+        // while that runs
+        let task_handle = Some(tokio::spawn(child));
 
-            if status.success() {
-                // The window/renderer process was closed normally
-                exit(0);
-            } else {
-                // Something went wrong, likely the other thread panicked
-                exit(1);
-            }
-        }));
+        // Keep a handle to the current runtime
+        let runtime_handle = runtime::Handle::current();
 
-        Self {task_handle, child_stdin}
+        Ok(Self {runtime_handle, task_handle, child_stdin})
     }
 
     /// Writes the given bytes followed by a newline b'\n' to the stdin of the process
@@ -76,26 +74,34 @@ impl Drop for RendererServerProcess {
     fn drop(&mut self) {
         use std::thread;
 
-        // If the current thread is panicking, we want to abort right away
-        // because otherwise there is code in the rendering thread that will call
-        // process::exit(0) and then the exit code will be 0 instead of 1
+        // If the current thread is panicking, we don't want to wait for the window to be closed
+        // since the user likely has an issue to fix in their program.
         if thread::panicking() {
-            exit(1);
+            return;
         }
 
         // If this is just a normal ending of the main thread, we want to leave the renderer
         // running so that the user can see their drawing as long as they keep the window open
-        // Need to spawn a new runtime because the current one would have already been dropped
-        let mut runtime = runtime::Builder::new()
-            .basic_scheduler()
-            .build()
-            .expect("unable to spawn tokio runtime to wait on turtle server process");
 
         // This unwrap is safe because no struct gets dropped twice
         let task_handle = self.task_handle.take().unwrap();
-        runtime.block_on(task_handle).unwrap_or_else(|_| {
-            // If this returns an error, the other thread panicked
-            exit(1);
-        });
+
+        // Wait for the child process to finish
+        match self.runtime_handle.block_on(task_handle) {
+            Ok(proc_status) => match proc_status {
+                Ok(proc_status) => if !proc_status.success() {
+                    // Propagate error code from child process or exit with status code 1
+                    process::exit(proc_status.code().unwrap_or(1));
+                },
+                // Error while awaiting the child process
+                Err(err) => {
+                    panic!("error while running child process: {}", err);
+                },
+            },
+            // Error caused by joining the task
+            Err(err) => {
+                panic!("error while waiting on task managing child process: {}", err);
+            },
+        }
     }
 }
