dac9e348782c61e15373cfa2efaa9da87ea5ea3f
Sunjay Varma <varma.sunjay@gmail.com>
2020-05-14 23:01:11 -0400

    Made rotation/speed tests a little more robust and printed actual timing information

--- examples/rotationtest2.rs
+++ examples/rotationtest2.rs
@@ -1,31 +1,40 @@
+//! This program demonstrates rotations at different speeds
+
+use std::time::Instant;
+
 use turtle::Turtle;
 
-/// This program demonstrates rotations at multiple speeds
 fn main() {
     let mut turtle = Turtle::new();
+
+    let levels = 25;
+    let test_area_width = 740.0;
 
     turtle.pen_up();
     turtle.set_speed("instant");
     turtle.left(90.0);
-    turtle.forward(300.0);
+    turtle.forward(test_area_width / 2.0);
     turtle.right(180.0);
     turtle.pen_down();
 
     turtle.hide();
-    for i in 1..12 {
+    for i in 1..=levels {
         turtle.set_speed(i);
+        let start = Instant::now();
         circle(&mut turtle);
+        let elapsed = start.elapsed();
+        println!("circle {}: {} ms", i, elapsed.as_millis());
 
         turtle.pen_up();
         turtle.set_speed("instant");
-        turtle.forward(60.0);
+        turtle.forward(test_area_width / levels as f64);
         turtle.pen_down();
     }
 }
 
 fn circle(turtle: &mut Turtle) {
-    for _ in 0..180 {
+    for _ in 0..90 {
         turtle.forward(1.0);
-        turtle.left(2.0);
+        turtle.left(4.0);
     }
 }
