6323c028ff86b6f64e4882e381f81343104931c8
Anett Seeker <noeddl@users.noreply.github.com>
2020-07-30 14:14:30 +0200

    Fix tests

--- src/color.rs
+++ src/color.rs
@@ -64,12 +64,12 @@ fn f64_eq(left: f64, right: f64) -> bool {
 /// You can refer to a color by using its color name as a string literal. For example:
 ///
 /// ```rust
-/// # use turtle::Color;
+/// # use turtle::{Color, color_consts};
 /// # let mut turtle = turtle::Turtle::new();
 /// // This will set the turtle's pen color to BLACK
 /// turtle.set_pen_color("black");
 /// // This is the same as the previous line
-/// turtle.set_pen_color(Color::BLACK);
+/// turtle.set_pen_color(color_consts::BLACK);
 /// // You can use any of the supported color names (including the ones from `extra_colors`)
 /// turtle.set_pen_color("deep lilac");
 /// ```
@@ -325,7 +325,7 @@ impl Color {
     /// * 0.0 &le; `alpha` &le; 1.0
     ///
     /// ```rust
-    /// use turtle::Color;
+    /// use turtle::{Color, color_consts};
     ///
     /// // You can chain using Color::from()
     /// let black = Color::from("black").with_alpha(0.5);
@@ -334,11 +334,11 @@ impl Color {
     ///
     /// // But even better, you can use the color enum value and chain the
     /// // calls.
-    /// let white = Color::WHITE.with_alpha(0.75);
+    /// let white = color_consts::WHITE.with_alpha(0.75);
     /// let white_hsla = Color::hsla(0.0, 1.0, 1.0, 0.75);
     /// assert_eq!(white, white_hsla);
     ///
-    /// let blue: Color = Color::BLUE.with_alpha(0.8);
+    /// let blue: Color = color_consts::BLUE.with_alpha(0.8);
     /// let blue_hsla = Color::hsla(201.0, 1.0, 0.392, 0.8);
     /// assert_eq!(blue, blue_hsla);
     /// ```
@@ -483,12 +483,12 @@ impl Color {
     /// Passing a value for `weight` that is not between 0.0 and 1.0 will result in a `panic`
     ///
     /// ```should_panic
-    /// use turtle::Color;
+    /// use turtle::{Color, color_consts};
     ///
     /// let orange: Color = "orange".into();
     ///
     /// // This will panic as 1.01 is not a valid value for weight, which must be between 0.0 and 1.0.
-    /// let mixed = orange.mix(Color::BROWN.with_alpha(0.8), 1.01);
+    /// let mixed = orange.mix(color_consts::BROWN.with_alpha(0.8), 1.01);
     /// ```
     ///
     /// # Example
--- src/rand.rs
+++ src/rand.rs
@@ -557,14 +557,14 @@ impl_random_slice!(32, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, 
 /// # Example
 ///
 /// ```rust,no_run
-/// use turtle::{Color, rand::shuffle};
+/// use turtle::{color_consts, rand::shuffle};
 ///
-/// let mut pen_colors = [Color::RED, Color::BLUE, Color::GREEN, Color::YELLOW];
+/// let mut pen_colors = [color_consts::RED, color_consts::BLUE, color_consts::GREEN, color_consts::YELLOW];
 /// // A different order of colors every time!
 /// shuffle(&mut pen_colors);
 ///
 /// // Even works with Vec
-/// let mut pen_colors = vec![Color::RED, Color::BLUE, Color::GREEN, Color::YELLOW];
+/// let mut pen_colors = vec![color_consts::RED, color_consts::BLUE, color_consts::GREEN, color_consts::YELLOW];
 /// shuffle(&mut pen_colors);
 /// ```
 pub fn shuffle<S: RandomSlice>(slice: &mut S) {
@@ -578,17 +578,17 @@ pub fn shuffle<S: RandomSlice>(slice: &mut S) {
 /// # Example
 ///
 /// ```rust,no_run
-/// use turtle::{Turtle, Color, rand::choose};
+/// use turtle::{Turtle, color_consts, rand::choose};
 ///
 /// let mut turtle = Turtle::new();
 ///
-/// let mut pen_colors = [Color::RED, Color::BLUE, Color::GREEN, Color::YELLOW];
+/// let mut pen_colors = [color_consts::RED, color_consts::BLUE, color_consts::GREEN, color_consts::YELLOW];
 /// // Choose a random pen color
 /// let chosen_color = choose(&mut pen_colors).cloned().unwrap();
 /// turtle.set_pen_color(chosen_color);
 ///
 /// // Even works with Vec
-/// let mut pen_colors = vec![Color::RED, Color::BLUE, Color::GREEN, Color::YELLOW];
+/// let mut pen_colors = vec![color_consts::RED, color_consts::BLUE, color_consts::GREEN, color_consts::YELLOW];
 /// let chosen_color = choose(&mut pen_colors).cloned().unwrap();
 /// turtle.set_pen_color(chosen_color);
 /// ```
