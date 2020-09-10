87fc7906f33cf10c3f4edc02d7d695425f8ce168
Paul Mabileau <paulmabileau@hotmail.fr>
2020-09-10 03:17:25 +0200

    Add back shell part of Ferris example
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- examples/ferris.rs
+++ examples/ferris.rs
@@ -91,10 +91,61 @@ fn main() {
     let ap = |point| adapt_point(point, size);
     let ad = |distance, angle| adapt_distance(distance, angle, size);
 
-    // turtle.bezier_rel(rp([
-    //     ap([]),
-    //     ap([]),
-    //     ap([]),
-    //     ap([]),
-    // ]));
+    let start_point = ap([240.0, 504.0]);
+    turtle.set_fill_color(BACK_SHELL_COLOR);
+    turtle.set_speed("instant");
+    turtle.turn_towards(start_point);
+    turtle.go_to(start_point);
+    turtle.set_speed("faster");
+    turtle.begin_fill();
+
+    let start_heading = (180.0 + 32.01f64).to_radians();
+    turtle.set_heading(start_heading);
+    turtle.forward(ad(18.9, start_heading));
+    turtle.bezier_rel(rp([
+        ap([223.64, 514.18]),
+        ap([222.18, 525.09]),
+        ap([288.00, 597.09]),
+        ap([323.94, 645.06]),
+    ]));
+    turtle.bezier_rel(rp([
+        ap([323.91, 645.18]),
+        ap([324.00, 623.09]),
+        ap([283.27, 565.27]),
+        ap([270.69, 529.94]),
+    ]));
+    turtle.bezier_rel(rp([
+        ap([270.75, 529.94]),
+        ap([444.75, 621.50]),
+        ap([826.25, 622.50]),
+        ap([973.09, 515.22]),
+    ]));
+    turtle.bezier_rel(rp([
+        ap([973.12, 515.25]),
+        ap([965.45, 552.18]),
+        ap([923.09, 619.45]),
+        ap([922.09, 646.52]),
+    ]));
+    turtle.bezier_rel(rp([
+        ap([922.38, 647.00]),
+        ap([931.09, 631.09]),
+        ap([988.91, 554.55]),
+        ap([1008.00, 517.12]),
+    ]));
+    let middle_curve_part1 = rp([
+        ap([1008.12, 517.06]),
+        ap([979.25, 500.88]),
+        ap([960.12, 507.19]),
+        ap([946.69, 510.31]),
+    ]);
+    turtle.bezier_rel(middle_curve_part1);
+    let middle_curve_part2 = rp([
+        ap([946.66, 510.41]),
+        ap([750.73, 606.91]),
+        ap([472.55, 603.45]),
+        ap([239.57, 505.39]),
+    ]);
+    turtle.bezier_rel(middle_curve_part2);
+
+    turtle.end_fill();
 }
