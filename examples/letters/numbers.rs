acddfa5de71d9182df58978dedfd3382d0552d2a
Juan Patricio Marchetto <juanpatriciomarchetto@gmail.com>
2021-03-03 19:13:09 -0300

    chore: ordering files and declarations

---
+++ examples/letters/numbers.rs
@@ -0,0 +1,14 @@
+use std::f64::consts::PI;
+use turtle::Turtle;
+
+// TODO: implement numbers
+pub fn zero(turtle: &mut Turtle, font_size: f64) {}
+pub fn one(turtle: &mut Turtle, font_size: f64) {}
+pub fn two(turtle: &mut Turtle, font_size: f64) {}
+pub fn three(turtle: &mut Turtle, font_size: f64) {}
+pub fn four(turtle: &mut Turtle, font_size: f64) {}
+pub fn five(turtle: &mut Turtle, font_size: f64) {}
+pub fn six(turtle: &mut Turtle, font_size: f64) {}
+pub fn seven(turtle: &mut Turtle, font_size: f64) {}
+pub fn eight(turtle: &mut Turtle, font_size: f64) {}
+pub fn nine(turtle: &mut Turtle, font_size: f64) {}
