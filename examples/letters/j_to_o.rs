60dba16506d1f943c802f0d281910190b2f976ff
Juan Patricio Marchetto <juanpatriciomarchetto@gmail.com>
2021-03-14 19:31:54 -0300

    optimizations

--- examples/letters/j_to_o.rs
+++ examples/letters/j_to_o.rs
@@ -6,8 +6,8 @@ use crate::diacritics::with_title;
 pub fn j(turtle: &mut Turtle, font_size: f64) {
     turtle.pen_up();
     turtle.forward(font_size / 3.0);
-    turtle.pen_down();
     turtle.left(180.0);
+    turtle.pen_down();
     for _ in 0..50 {
         turtle.forward(PI / 150.0 * font_size);
         turtle.left(3.6);
@@ -16,33 +16,35 @@ pub fn j(turtle: &mut Turtle, font_size: f64) {
     turtle.pen_up();
     turtle.backward(font_size);
     turtle.right(90.0);
-    turtle.forward(font_size / 2.0);
+    turtle.forward(font_size / 4.0);
     turtle.left(90.0);
 }
 
 pub fn k(turtle: &mut Turtle, font_size: f64) {
     turtle.forward(font_size);
+    turtle.pen_up();
     turtle.right(90.0);
-    turtle.pen_up();
-    turtle.forward(font_size * 0.88);
-    turtle.pen_down();
-    turtle.left(30.0);
-    turtle.backward(1.25_f64.sqrt() * font_size * 0.9);
-    turtle.right(60.0);
-    turtle.forward(1.25_f64.sqrt() * font_size * 0.9);
-    turtle.left(30.0);
-    turtle.pen_up();
     turtle.forward(font_size / 2.0);
+    turtle.left(45.0);
+    turtle.pen_down();
+    turtle.backward(2_f64.sqrt() * font_size / 2.0);
+    turtle.right(90.0);
+    turtle.forward(2_f64.sqrt() * font_size / 2.0);
+    turtle.left(45.0);
+    turtle.pen_up();
+    turtle.forward(font_size / 4.0);
     turtle.left(90.0);
 }
 
 pub fn l(turtle: &mut Turtle, font_size: f64) {
     turtle.forward(font_size);
+    turtle.pen_up();
     turtle.backward(font_size);
     turtle.right(90.0);
+    turtle.pen_down();
     turtle.forward(font_size / 2.0);
     turtle.pen_up();
-    turtle.forward(font_size / 2.0);
+    turtle.forward(font_size / 4.0);
     turtle.left(90.0);
 }
 
@@ -54,9 +56,9 @@ pub fn m(turtle: &mut Turtle, font_size: f64) {
     turtle.forward(1.25_f64.sqrt() * font_size);
     turtle.left(30.0);
     turtle.backward(font_size);
-    turtle.right(90.0);
     turtle.pen_up();
-    turtle.forward(font_size / 2.0);
+    turtle.right(90.0);
+    turtle.forward(font_size / 4.0);
     turtle.left(90.0);
 }
 
@@ -69,7 +71,7 @@ pub fn n(turtle: &mut Turtle, font_size: f64) {
     turtle.pen_up();
     turtle.backward(font_size);
     turtle.right(90.0);
-    turtle.forward(font_size / 2.0);
+    turtle.forward(font_size / 4.0);
     turtle.left(90.0);
 }
 
@@ -79,8 +81,8 @@ pub fn enie(mut turtle: &mut Turtle, font_size: f64) {
 }
 
 pub fn o(turtle: &mut Turtle, font_size: f64) {
-    turtle.right(90.0);
     turtle.pen_up();
+    turtle.right(90.0);
     turtle.forward(0.5 * font_size);
     turtle.pen_down();
     for _ in 0..100 {
@@ -88,6 +90,6 @@ pub fn o(turtle: &mut Turtle, font_size: f64) {
         turtle.left(3.6);
     }
     turtle.pen_up();
-    turtle.forward(font_size);
+    turtle.forward(font_size * 3.0 / 4.0);
     turtle.left(90.0);
 }
--- examples/letters/u_to_z.rs
+++ examples/letters/u_to_z.rs
@@ -6,17 +6,15 @@ pub fn u(turtle: &mut Turtle, font_size: f64) {
     turtle.forward(font_size);
     turtle.pen_down();
     turtle.backward(font_size / 2.0);
-    turtle.left(180.0);
     for _ in 0..50 {
-        turtle.forward(PI / 100.0 * font_size);
+        turtle.backward(PI / 100.0 * font_size);
         turtle.left(3.6);
     }
-    turtle.forward(font_size / 2.0);
+    turtle.backward(font_size / 2.0);
     turtle.pen_up();
-    turtle.left(180.0);
     turtle.forward(font_size);
     turtle.left(90.0);
-    turtle.forward(font_size / 2.0);
+    turtle.forward(font_size / 4.0);
     turtle.left(90.0);
 }
 
@@ -32,7 +30,7 @@ pub fn v(turtle: &mut Turtle, font_size: f64) {
     turtle.left(30.0);
     turtle.backward(font_size);
     turtle.right(90.0);
-    turtle.forward(font_size / 2.0);
+    turtle.forward(font_size / 4.0);
     turtle.left(90.0);
 }
 
@@ -60,15 +58,14 @@ pub fn x(turtle: &mut Turtle, font_size: f64) {
     turtle.right(45.0);
     turtle.forward(2.0_f64.sqrt() * font_size);
     turtle.pen_up();
-    turtle.left(45.0);
+    turtle.right(45.0);
     turtle.backward(font_size);
-    turtle.left(45.0);
+    turtle.right(45.0);
     turtle.pen_down();
     turtle.forward(2.0_f64.sqrt() * font_size);
     turtle.pen_up();
-    turtle.backward(2.0_f64.sqrt() * font_size);
-    turtle.right(135.0);
-    turtle.forward(font_size / 2.0);
+    turtle.left(45.0);
+    turtle.forward(font_size / 4.0);
     turtle.left(90.0);
 }
 
@@ -83,7 +80,7 @@ pub fn y(turtle: &mut Turtle, font_size: f64) {
     turtle.pen_up();
     turtle.backward(1.25 * font_size);
     turtle.right(120.0);
-    turtle.forward(font_size / 2.0);
+    turtle.forward(font_size / 4.0);
     turtle.left(90.0);
 }
 
@@ -107,6 +104,6 @@ pub fn z(turtle: &mut Turtle, font_size: f64) {
     turtle.pen_down();
     turtle.forward(font_size);
     turtle.pen_up();
-    turtle.backward(font_size * 1.50);
+    turtle.backward(font_size * 5.0 / 4.0);
     turtle.right(90.0);
 }
