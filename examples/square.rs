dcdf18b36e30390f3ee3046c6ae831c9c6099801
Sathwik Matsa <sathwikmatsa@gmail.com>
2021-07-24 20:08:20 +0530

    revert changes in square.rs

--- examples/square.rs
+++ examples/square.rs
@@ -1,17 +1,10 @@
-use turtle::*;
+use turtle::Turtle;
 
 fn main() {
     let mut turtle = Turtle::new();
 
-    turtle.pen_up();
-    turtle.go_to((-100.0, -100.0));
-    turtle.pen_down();
-    turtle.begin_fill();
-    turtle.set_pen_color("green");
-    turtle.set_fill_color("green");
     for _ in 0..4 {
         turtle.forward(200.0);
         turtle.right(90.0);
     }
-    turtle.end_fill();
 }
